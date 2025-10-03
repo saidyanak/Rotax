39581c37afdb9106c31c2f857dbf4a64cca27e44
said yanak <75083985+saidyanak@users.noreply.github.com>
2025-10-03 15:26:46 +0300

    The project was created

---
+++ Rotax/src/main/java/com/hilgo/rotax/RotaxApplication.java
@@ -0,0 +1,13 @@
+package com.hilgo.rotax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RotaxApplication.class, args);
+    }
+
+}
---
+++ Rotax/src/test/java/com/hilgo/rotax/RotaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilgo.rotax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
